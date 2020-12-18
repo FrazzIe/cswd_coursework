fedc992515616c161a8746b97193d1a37fbfc388
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-12-18 06:45:28 +0000

    add book view area

--- pages/books/_id.vue
+++ pages/books/_id.vue
@@ -1,7 +1,42 @@
 <template>
-	<v-row justify="center" align="center">
-		<v-col sm="7" md="6" lg="5" xl="4">
-			<v-card class="elevation-12" tile>
+	<v-row justify="center">
+		<v-col sm="7" md="6" lg="5" xl="5">
+			<v-card class="elevation-12">
+				<book-cover :isbn="book.isbn" height="400px"></book-cover>
+				<v-divider></v-divider>
+				<v-card-title>{{ book.title }}</v-card-title>
+				<v-card-text>
+					<v-row justify="space-between">
+						<v-col sm="6">
+							<p class="text-subtitle-1 font-weight-black">
+								<span class="text-subtitle-2">Author(s)</span>
+								<br/>
+								{{ book.authors }}
+								<br/>
+								<span class="text-subtitle-2">ISBN-13</span>
+								<br/>
+								{{ book.isbn }}
+							</p>
+						</v-col>
+						<v-col sm="6">
+							<p class="text-subtitle-1 font-weight-black">
+								<span class="text-subtitle-2">Category</span>
+								<br/>
+								{{ book.category }}
+								<br/>
+								<span class="text-subtitle-2">Release date</span>
+								<br/>
+								{{ book.year }}
+							</p>
+						</v-col>
+						<v-col cols="12">
+							<span class="text-subtitle-2">Description</span>
+							<p class="text-justify">{{ book.description }}</p>
+						</v-col>
+					</v-row>
+				</v-card-text>
+			</v-card>
+		</v-col>
 
 			</v-card>
 		</v-col>
